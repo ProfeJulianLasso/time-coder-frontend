4eaf3c9e5f0a3b5e7edb5842f4970bb4e45da5b6
Julian Lasso <julian.lasso@sofka.com.co>
2025-03-12 16:21:57 -0500

    feat: :sparkles: configurar el servidor y la vista previa para permitir conexiones en hosts específicos

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    port: 8080,
+    host: true,
+    strictPort: true,
+    allowedHosts: ["timecoder.dev", "www.timecoder.dev", "localhost"],
+  },
+  server: {
+    host: true,
+    port: 8080,
+  },
 });
